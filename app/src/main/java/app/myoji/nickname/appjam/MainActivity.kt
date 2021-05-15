47edd385f595ae0dfbd7d22e68840b0c39d73223
mina1112 <mina.kato8885@gmail.com>
2021-05-15 22:38:27 +0900

    first commit

--- app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/appjam/MainActivity.kt
@@ -1,11 +1,12 @@
 package app.myoji.nickname.appjam
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 //残り時間のセット
@@ -15,40 +16,28 @@ class MainActivity : AppCompatActivity() {
         //タイマーが終了するときに呼ばれる
         override fun onFinish() {
             //スタートボタンを表示する
-            startButton.isVisible = true
+            startButton2.isVisible = true
             //残り時間をリセットする
             second = 10
             //時間の表示をもとに戻す
-            secondText.text = second.toString()
+            secondText2.text = second.toString()
         }
 
     override fun onTick(millisUntilFinished: Long) {
         //残り時間を1秒ずつ減らす
         second = second - 1
         //残り時間を表示する
-        secondText.text = second.toString()
+        secondText2.text = second.toString()
     }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //残り時間を表示
-        secondText.text = second.toString()
 
-        secondText.isVisible = false
-        timeLabelText.isVisible = false
+        val quizIntent: Intent = Intent(this, QuizActivity::class.java)
 
-        //スタートボタンが押されたとき、スタートボタンを見えない状態にする
-        startButton.setOnClickListener {
-
-            startButton.isVisible = false
-
-            secondText.isVisible = true
-            timeLabelText.isVisible = true
-
-            //タイマーを開始する
-            timer.start()
+      
         }
 
 
---
+++ app/src/main/java/app/myoji/nickname/appjam/QuizActivity.kt
@@ -0,0 +1,21 @@
+package app.myoji.nickname.appjam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    }
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+
+    }
+}
